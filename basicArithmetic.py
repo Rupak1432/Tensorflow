9743878ec4fe788888d9f54e19a60a03b9aea151
rupak1432 <rupakkrishnamurthy@gmail.com>
2018-06-02 10:37:41 +0530

    difference in prints

--- basicArithmetic.py
+++ basicArithmetic.py
@@ -3,6 +3,13 @@ import tensorflow as tf
 a = tf.constant(2)
 b = tf.constant(3)
 
+c = tf.multiply(a,b)
+
+print(c)
+
 with tf.Session() as sess:
     print("Addition",sess.run(a+b))
     print("Multiplication",sess.run(a*b))
+    print("c",sess.run(c))
+
+sess.close()
