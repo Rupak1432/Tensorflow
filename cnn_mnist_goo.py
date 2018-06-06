77b529b87ff7d3b0c336334c17818dede45dc3e8
rupak1432 <rupakkrishnamurthy@gmail.com>
2018-06-06 14:54:16 +0530

    Added Cnn model of MNIST by Tensorflow

--- cnn_mnist_goo.py
+++ cnn_mnist_goo.py
@@ -1,126 +1,111 @@
-# https://www.tensorflow.org/tutorials/layers
-from __future__ import print_function, division, absolute_import
-import time
-start_time = time.time()
-print("\nstart ", start_time, "\n")
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
+# Imports
 import numpy as np
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
-#from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
 
-#tf.logging.set_verbosity(tf.logging.INFO)
+tf.logging.set_verbosity(tf.logging.INFO)
 
 def cnn_model_fn(features, labels, mode):
-    
-    input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
+  """Model function for CNN."""
+  # Input Layer
+  input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
 
-    conv1 = tf.layers.conv2d(
-        inputs = input_layer,
-        filters = 32,
-        kernel_size = [5,5],
-        padding="same",
-        activation=tf.nn.relu
-    )
+  # Convolutional Layer #1
+  conv1 = tf.layers.conv2d(
+      inputs=input_layer,
+      filters=32,
+      kernel_size=[5, 5],
+      padding="same",
+      activation=tf.nn.relu)
 
-    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2,2], strides=2)
+  # Pooling Layer #1
+  pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
 
-    conv2 = tf.layers.conv2d(
-        inputs=pool1,
-        filters=64,
-        kernel_size=[5,5],
-        padding="same",
-        activation=tf.nn.relu
-    )
+  # Convolutional Layer #2 and Pooling Layer #2
+  conv2 = tf.layers.conv2d(
+      inputs=pool1,
+      filters=64,
+      kernel_size=[5, 5],
+      padding="same",
+      activation=tf.nn.relu)
+  pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
 
-    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2,2], strides=2)
+  # Dense Layer
+  pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
+  dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+  dropout = tf.layers.dropout(
+      inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
 
-    pool2_flat = tf.reshape(pool2, [-1, 7*7*64])
-    dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
-    dropout = tf.layers.dropout(inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
+  # Logits Layer
+  logits = tf.layers.dense(inputs=dropout, units=10)
 
-    logits = tf.layers.dense(inputs=dropout, units=10)
-    #print(np.shape(logits)) -- (100, 10)
-    predictions = {
-        "classes": tf.argmax(input=logits, axis=1),
-        "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
-    }    
+  predictions = {
+      # Generate predictions (for PREDICT and EVAL mode)
+      "classes": tf.argmax(input=logits, axis=1),
+      # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
+      # `logging_hook`.
+      "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+  }
 
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
-    
-    onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=10)
-    #print(np.shape(onehot_labels)) -- (100, 10, 10)
-    loss = tf.losses.softmax_cross_entropy(onehot_labels=onehot_labels, logits=logits)
-    #loss = tf.losses.softmax_cross_entropy(onehot_labels=labels, logits=logits)
+  if mode == tf.estimator.ModeKeys.PREDICT:
+    return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
 
-    if mode == tf.estimator.ModeKeys.TRAIN:
-        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
-        train_op = optimizer.minimize(
-            loss = loss,
-            global_step = tf.train.get_global_step()
-        )
-        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+  # Calculate Loss (for both TRAIN and EVAL modes)
+  loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
 
-    eval_metric_ops = {
-        "accuracy": tf.metrics.accuracy(
-            labels = loss, predictions = predictions["classes"]
-        )
-    }
+  # Configure the Training Op (for TRAIN mode)
+  if mode == tf.estimator.ModeKeys.TRAIN:
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+    train_op = optimizer.minimize(
+        loss=loss,
+        global_step=tf.train.get_global_step())
+    return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
 
-    return tf.estimator.EstimatorSpec(mode = mode, loss = loss, eval_metric_ops=eval_metric_ops)
-
+  # Add evaluation metrics (for EVAL mode)
+  eval_metric_ops = {
+      "accuracy": tf.metrics.accuracy(
+          labels=labels, predictions=predictions["classes"])}
+  return tf.estimator.EstimatorSpec(
+      mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 def main(unused_argv):
-    #mnist = tf.contrib.learn.datasets.load_dataset("mnist")
-    mnist = input_data.read_data_sets("MNIST_data/", one_hot=False)
-    #mnist = read_data_sets("/home/rupak/Desktop/Tensorflow/", one_hot=False)
-    train_data = mnist.train.images
-    train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
-    #print(np.shape(train_labels))
-    eval_data = mnist.test.images
-    eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
-    #print(np.shape(eval_labels))
+  # Load training and eval data
+  mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+  train_data = mnist.train.images # Returns np.array
+  train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
+  eval_data = mnist.test.images # Returns np.array
+  eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
+  # Create the Estimator
+  mnist_classifier = tf.estimator.Estimator(
+    model_fn=cnn_model_fn, model_dir="/home/rupak/Desktop/Tensorflow/log/")
+  # Set up logging for predictions
+  tensors_to_log = {"probabilities": "softmax_tensor"}
+  logging_hook = tf.train.LoggingTensorHook(
+      tensors=tensors_to_log, every_n_iter=50)
+  # Train the model
+  train_input_fn = tf.estimator.inputs.numpy_input_fn(
+    x={"x": train_data},
+    y=train_labels,
+    batch_size=100,
+    num_epochs=None,
+    shuffle=True)
+  mnist_classifier.train(
+    input_fn=train_input_fn,
+    steps=20000,
+    hooks=[logging_hook])
+  # Evaluate the model and print results
+  eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+    x={"x": eval_data},
+    y=eval_labels,
+    num_epochs=1,
+    shuffle=False)
+  eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
+  print(eval_results)
 
-    mnist_classifier = tf.estimator.Estimator(model_fn=cnn_model_fn, model_dir="/home/rupak/Desktop/Tensorflow")
 
-    tensors_to_log = {"probabilities": "softmax_tensor"}
-    logging_hook = tf.train.LoggingTensorHook(tensors=tensors_to_log, every_n_iter=50)
 
-    train_input_fn = tf.estimator.inputs.numpy_input_fn(
-        x = {"x": train_data},
-        y = train_labels,
-        batch_size=100,
-        num_epochs=None,
-        shuffle=True
-    )
-
-    mnist_classifier.train(
-        input_fn = train_input_fn,
-        steps = 2000,
-        hooks=[logging_hook]
-    )
-
-    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
-        x = {"x": eval_data},
-        y = eval_labels,
-        num_epochs=1,
-        shuffle=False
-    )
-
-    eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
-    #print(eval_results)
-
-    
 
 if __name__ == "__main__":
-
-    #start_time = time.time()
-    #print("Mnist CNN started at ",start_time)
-
-    tf.app.run()
-
-    end_time = time.time()
-    #print("The program ended at ",end_time)
-    #print("Time taken ",(start_time-end_time)/60," Minutes")
-
-print("\nTime taken ", (start_time-time.time())/60, " Minutes\n")
+  tf.app.run()
